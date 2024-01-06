fb34aedd9c9b4777deb6881aefc5c8efd0376593
ajwadtahmid <dev@ajwadtahmid.com>
2024-01-05 20:09:40 -0800

    changes to style

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,7 +4,11 @@ import "./projects.css"
 const Projects = () => {
   return (
     <section id="projects">
-        
+        <div className="projectSection">
+          <div className="projectTitle">
+            
+          </div>
+        </div>
     </section>
   )
 }
