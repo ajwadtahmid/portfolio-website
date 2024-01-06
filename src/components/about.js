6e0c7588fcc2e3426d901e5ec9c5824a1c85a619
ajwadtahmid <dev@ajwadtahmid.com>
2024-01-05 21:48:56 -0800

    changed font style and weight

--- src/components/about.js
+++ src/components/about.js
@@ -7,7 +7,7 @@ const About = () => {
         <div className="aboutContent">
             <span className="hello">Hello,<br/></span>
             <span className="introText">I'm <span className="introName">Ajwad</span><br/></span>
-            <p className="aboutParagraph">....</p>
+            <p className="aboutParagraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem dolor sed viverra ipsum nunc aliquet. Mi proin sed libero enim sed faucibus turpis in. Felis imperdiet proin fermentum leo vel orci. Non curabitur gravida arcu ac tortor dignissim convallis. Ut tellus elementum sagittis vitae. At augue eget arcu dictum. Tristique magna sit amet purus gravida quis blandit. Convallis tellus id interdum velit laoreet id donec ultrices tincidunt. Tincidunt lobortis feugiat vivamus at augue eget. Sagittis eu volutpat odio facilisis. Ac turpis egestas integer eget aliquet nibh. Morbi non arcu risus quis varius quam quisque. Sed viverra tellus in hac habitasse platea dictumst. Pretium aenean pharetra magna ac placerat vestibulum.</p>
         </div>
     </section>
   )
