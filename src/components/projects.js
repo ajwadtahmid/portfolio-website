5ed7070e111f5afe27a8d8539c39c385d88b0eed
ajwadtahmid <dev@ajwadtahmid.com>
2024-01-05 21:29:39 -0800

    style changes

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,8 +5,26 @@ const Projects = () => {
   return (
     <section id="projects">
         <div className="projectSection">
-          <div className="projectTitle">
-            
+          <h1 className="projectSectionTitle">Projects</h1>
+          <div className="projectBars">
+            <a href="https://github.com/ajwadtahmid/portfolio-website" className="projectBar" target="_blank" rel="noopener noreferrer">
+              <div className="projectBarTitle">
+                <h2>Portfolio Website</h2>
+              </div>
+              <div className="projectBarDesc">
+                <p>Used HTML, CSS, JavaScript and React to create the website you are viewing right now</p>
+              </div>
+            </a>
+          </div>
+          <div className="projectBars">
+            <a href="https://github.com/ajwadtahmid/TipCalculator" className="projectBar" target="_blank" rel="noopener noreferrer">
+              <div className="projectBarTitle">
+                <h2>Tip Calculator (Android)</h2>
+              </div>
+              <div className="projectBarDesc">
+                <p> Created a tip calcuator in kotlin that updates result in real time.</p>
+              </div>
+            </a>
           </div>
         </div>
     </section>
